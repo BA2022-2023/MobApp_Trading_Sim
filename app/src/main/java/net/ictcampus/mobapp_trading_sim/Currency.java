283aad55beb0ca910c2768926d8fa81544e50adf
Future_mr_cool <kofmehl.lennox@gmx.de>
2023-05-04 15:21:10 +0200

    gps

--- app/src/main/java/net/ictcampus/mobapp_trading_sim/Currency.java
+++ app/src/main/java/net/ictcampus/mobapp_trading_sim/Currency.java
@@ -1,78 +1,32 @@
 package net.ictcampus.mobapp_trading_sim;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Arrays;
-
-
 public class Currency extends AppCompatActivity {
-    ArrayAdapter badiListe;
-    private final static String AARBERG = "Schwimmbad Aarberg (BE)";
-    private final static String ADELBODEN = "Schwimmbad Gruebi Adelboden (BE)";
-    private final static String GRENCHEN = "Schwimmbad Grenchen (SO)";
-    private final static String ZHSEE = "Zürichsee (ZH)";
+        TextView tv_lat,tv_lon,tv_altitude,tv_speed,tv_sensor,tv_updates,tv_address;
+        Switch sw_locationupdates, sw_gps;
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_currency);
 
-    private void addBadisToList() {
-        ListView badis = findViewById(R.id.badiListe);
-        badiListe = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        for (String s : Arrays.asList(AARBERG, ADELBODEN, GRENCHEN, ZHSEE)) {
-            badiListe.add(s);
+            tv_lat = findViewById(R.id.tv_lat);
+            tv_lon = findViewById(R.id.tv_lon);
+            tv_altitude = findViewById(R.id.tv_altitude);
+            tv_speed = findViewById(R.id.tv_speed);
+            tv_sensor = findViewById(R.id.tv_sensor);
+            tv_updates = findViewById(R.id.tv_updates);
+            tv_address = findViewById(R.id.tv_address);
+            sw_gps = findViewById(R.id.sw_gps);
+            sw_locationupdates = findViewById(R.id.sw_locationsupdates);
+
+
+
         }
-        badis.setAdapter(badiListe);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currency);
-
-
-
-        addBadisToList();
-
-
-        //KlickListener in anonymer Klasse an Adapter hängen
-        AdapterView.OnItemClickListener mListClickHandler = new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), BadiDetailsActivity.class);
-                String selected = parent.getItemAtPosition(position).toString();
-//Kleine Infobox, dass der Klick empfangen wurde
-                Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
-                //Zusatzinformationen an den intent (andere Activity), welche Badi ID
-                switch (selected) {
-                    case AARBERG:
-                        intent.putExtra("badi", "71");
-                        break;
-                    case ADELBODEN:
-                        intent.putExtra("badi", "27");
-                        break;
-                    case GRENCHEN:
-                        intent.putExtra("badi", "174");
-                        break;
-                    case ZHSEE:
-                        intent.putExtra("badi", "55");
-                        break;
-                }
-                intent.putExtra("name", selected);
-                startActivity(intent);
-
-            }
-        };
-        //Listener an ListView hängen
-        ListView badis = findViewById(R.id.badiListe);
-        badis.setOnItemClickListener(mListClickHandler);
-
-    }
-
 
 }
