0c0e43f20d1893169f2216d83a628fcf2fb1a2c4
Future_mr_cool <kofmehl.lennox@gmx.de>
2023-05-05 12:26:39 +0200

    gps

--- app/src/main/java/net/ictcampus/mobapp_trading_sim/Currency.java
+++ app/src/main/java/net/ictcampus/mobapp_trading_sim/Currency.java
@@ -1,7 +1,9 @@
 package net.ictcampus.mobapp_trading_sim;
 
 
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -9,35 +11,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class Currency extends AppCompatActivity {
-        TextView tv_lat,tv_lon,tv_altitude,tv_speed,tv_sensor,tv_updates,tv_address;
-        Switch sw_locationupdates, sw_gps;
+    TextView tv_lat,tv_lon,tv_altitude,tv_speed,tv_sensor,tv_updates,tv_address;
+    Switch sw_locationupdates, sw_gps;
 
-        boolean updateOn = false;
+    boolean updateOn = false;
 
-        LocationRequest locationRequest;
-        FusedLocationProviderClient fusedLocationProvider;
+    LocationRequest locationRequest = new LocationRequest();
+    FusedLocationProviderClient fusedLocationProvider;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_currency);
 
-            tv_lat = findViewById(R.id.tv_lat);
-            tv_lon = findViewById(R.id.tv_lon);
-            tv_altitude = findViewById(R.id.tv_altitude);
-            tv_speed = findViewById(R.id.tv_speed);
-            tv_sensor = findViewById(R.id.tv_sensor);
-            tv_updates = findViewById(R.id.tv_updates);
-            tv_address = findViewById(R.id.tv_address);
-            sw_gps = findViewById(R.id.sw_gps);
-            sw_locationupdates = findViewById(R.id.sw_locationsupdates);
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
 
-            locationRequest = new LocationRequest();
-            locationRequest.setInterval(1000 * 30);
-            locationRequest.setFastestInterval(1000 * 5);
-        }
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        sw_gps = findViewById(R.id.sw_gps);
+        sw_locationupdates = findViewById(R.id.sw_locationsupdates);
+
+
+        locationRequest.setInterval(1000 * 30);
+        locationRequest.setFastestInterval(1000 * 5);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    sw_gps.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v){
+           if(sw_gps.isChecked()){
+               locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+               tv_sensor.setText("Using GPS sensors");
+           }else {
+               locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+               tv_sensor.setText("Using Towers 1 Wifi");
+           }
+       }
+    });
 
 }
