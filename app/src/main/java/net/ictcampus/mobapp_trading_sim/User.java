f1c8fa32737f8387d6e024baf724bf6682accae6
kellerlev <levin.keller@ict-campus.net>
2023-05-11 10:31:07 +0200

    Databse added

--- app/src/main/java/net/ictcampus/mobapp_trading_sim/User.java
+++ app/src/main/java/net/ictcampus/mobapp_trading_sim/User.java
@@ -1,8 +1,23 @@
 package net.ictcampus.mobapp_trading_sim;
 
-public class User {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity
+public class User {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+
+    @ColumnInfo(name = "username")
     String username;
+    @ColumnInfo(name = "password")
     String password;
 
     public User() {
